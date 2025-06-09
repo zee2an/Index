0ec658b133f34d3153542314c0286b30a985132f
zee2an <153084368+zee2an@users.noreply.github.com>
2025-06-09 07:35:04 +0500

    Update and rename script.js to Server.js

--- Server.js
+++ Server.js
@@ -4,6 +4,13 @@ const cheerio = require('cheerio');
 
 const app = express();
 
+app.use(express.json()); // parse JSON bodies
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
 app.post('/scrape', (req, res) => {
     const url = req.body.url;
     axios.get(url)
@@ -19,4 +26,8 @@ app.post('/scrape', (req, res) => {
         res.json(images);
     })
     .catch((error) => console.error(error));
+});
+
+app.listen(3000, () => {
+    console.log('Server listening on port 3000');
 });
